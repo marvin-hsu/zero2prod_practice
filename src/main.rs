74c4504dd803a2b09ed016d997e11ea0f2886f14
marvin <marv.hsu@icloud.com>
2022-12-13 23:53:45 +0800

    add health_check function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("0.0.0.0:8000")?
     .run()
     .await
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder{
+    HttpResponse::Ok()
 }
