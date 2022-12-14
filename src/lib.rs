0d6772bff017346b3dd5c88b68d3bb3ad1e3d59d
marvin <marv.hsu@icloud.com>
2022-12-14 22:38:03 +0800

    add subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,28 @@ use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
-pub fn run(listener:TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
-        .route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
 
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -8,7 +8,7 @@ async fn health_check_work() {
 
     // Act
     let response = client
-        .get(format!("{}/health_check",address))
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute requests.");
@@ -18,11 +18,56 @@ async fn health_check_work() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=hsu%20marvin&email=marvin_hsu%40gmail.com";
+
+    // Act
+    let response = client
+        .post(format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute requests.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=hsu%20marvin", "missing the email"),
+        ("email=marvin_hsu%40gmail.com", "missing the name"),
+        ("", "missing both name the email"),
+    ];
+
+    // Act
+    for (invalid_body, error_msg) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute requests.");
+
+        // Assert
+        assert_eq!(400, response.status().as_u16())
+    }
+}
+
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("0.0.0.0:0")
-    .expect("Failed to bind random port");
+    let listener = TcpListener::bind("0.0.0.0:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod_practice::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://0.0.0:{}",port)
+    format!("http://0.0.0:{}", port)
 }
