91ea4f4ddeb351b42aeef54edd95cc42b57c2488
marvin <marv.hsu@icloud.com>
2022-12-19 21:28:53 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use tracing::Subscriber;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod_practice::configuration::get_configuration;
 use zero2prod_practice::startup::run;
+use zero2prod_practice::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -22,19 +18,4 @@ async fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
-}
-
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
-LogTracer::init().expect("Failed to set logger");
-set_global_default(subscriber).expect("Failed to set subscriber");
-}
-
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
 }
