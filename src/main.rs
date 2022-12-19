77cfec2c2bbdeb02f40dbfc55500dc1c59334b36
marvin <marv.hsu@icloud.com>
2022-12-19 21:10:21 +0800

    add log

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection, PgPool};
+use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod_practice::configuration::get_configuration;
 use zero2prod_practice::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to set logger");
+    
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connection to Postgres.");
 
     let address = format!("0.0.0.0:{}", configuration.application_port);
