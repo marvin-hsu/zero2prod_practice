e1b76c7344e8f96cc12f0ba6259b32ec27aeda1a
marvin <marv.hsu@icloud.com>
2022-12-19 21:22:28 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use tracing::Subscriber;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -10,15 +11,8 @@ use zero2prod_practice::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-    
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
@@ -28,4 +22,19 @@ async fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+LogTracer::init().expect("Failed to set logger");
+set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
 }
