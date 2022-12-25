af819f85ef077afa61957f4254363e60bea5e84f
marvin <marv.hsu@icloud.com>
2022-12-25 12:13:07 +0800

    refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -20,24 +20,25 @@ subscriber_name= %_form.name
 )
 )]
 pub async fn subscribe(_form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(_form.0.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
 
-    let email = match SubscriberEmail::parse(_form.0.email) {
-        Ok(email)=> email,
-        Err(_)=> return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber {
-        email: email,
-        name: name,
+    let new_subscriber = match _form.0.try_into() {
+        Ok(subscriber)=> subscriber,
+        Err(_)=> return HttpResponse::BadRequest().finish()        
     };
 
     match insert_subscriber(&db_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error=String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+    let name = SubscriberName::parse(value.name)?;
+    let email = SubscriberEmail::parse(value.email)?;
+    Ok(NewSubscriber { email: email, name: name })
     }
 }
 
